9fcc07c08dcf9720c527c51a2a1911fbe8e0162a
Michaela Meitnerová <meitnerova@SPSEJECNANET.CZ>
2023-05-16 08:37:44 +0200

    Pridani projektu

---
+++ src/Entita.java
@@ -0,0 +1,27 @@
+
+public class Entita {
+
+    private String jmeno;
+    private int cislo;
+
+
+    /**
+     *
+     * @return
+     */
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public int getCislo() {
+        return cislo;
+    }
+
+    public void setCislo(int cislo) {
+        this.cislo = cislo;
+    }
+}
